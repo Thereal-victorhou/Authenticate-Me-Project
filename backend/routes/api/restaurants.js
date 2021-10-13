1c960e5803a79997abc26be1e78e9644f4243481
Victor Hou <hou_v@yahoo.com>
2021-10-13 11:10:45 -0700

    got comments to render, debugging issue where restaurant isn't found

--- backend/routes/api/restaurants.js
+++ backend/routes/api/restaurants.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
+// const Op = Sequelize.Op
 
 const { Restaurant } = require('../../db/models');
+const { Review } = require('../../db/models');
 
 const router = express.Router();
 
@@ -12,8 +14,15 @@ router.get('/', asyncHandler(async(req, res)=> {
 
 router.get('/:id', asyncHandler(async(req, res)=> {
     const id = parseInt(req.params.id, 10);
-    const restaurant = await Restaurant.findByPk(id);
-    return res.json(restaurant);
+    const restaurant = await Restaurant.findByPk(id, {
+        include: [{
+            model: Review,
+            required: true,
+            where: { restaurantId: id },
+        }],
+    });
+    const data = res.json(restaurant);
+    console.log(data);
 }));
 
 module.exports = router;
