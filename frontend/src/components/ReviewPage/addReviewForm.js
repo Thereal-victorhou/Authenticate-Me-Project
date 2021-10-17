482c9d55adfbd64ae803488b1396204224f78791
Victor Hou <hou_v@yahoo.com>
2021-10-17 11:57:41 -0700

    worked on making star inputs for addReviewForm

--- frontend/src/components/ReviewPage/addReviewForm.js
+++ frontend/src/components/ReviewPage/addReviewForm.js
@@ -9,31 +9,56 @@ function AddReviewForm({ user }) {
     const { id } = useParams();
     const restaurantId = id;
     const [body, setBody] = useState("");
+    const [rating, setRating] = useState(0);
     const updateBody = (e) => setBody(e.target.value)
 
     const userId = user.id;
 
-    // useEffect(()=> {
-    //     setBody(body)
-    // }, [dispatch, body]);
+    useEffect(()=> {}, [dispatch, rating]);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
 
         const reviewPayload = {
             body,
-            restaurantId
+            restaurantId,
+            rating
         };
 
         await dispatch(newReview(reviewPayload, userId));
         history.push(`/restaurants/${id}`);
     }
 
+    const handleStars = async(e) => {
+        e.preventDefault();
+        switch(e.target.value) {
+            case 1:
+                setRating(1);
+                console.log(rating)
+            case 2:
+                return setRating(e.target.value + 2);
+            case 3:
+                return setRating(e.target.value + 3);
+            case 4:
+                return setRating(e.target.value + 4);
+            case 5:
+                return setRating(e.target.value + 5);
+        }
+    }
+
+  console.log(rating)
+
     return(
         <>
             <h1>Hello from Add Review Page</h1>
             <section>
                 <form onSubmit={handleSubmit}>
+                    <div className='stars_container_review_page'></div>
+                        <button type='button' className='star' id='one' value={rating} onClick={handleStars}>★</button>
+                        <button className='star' id='two' value={rating} onClick={handleStars}>★</button>
+                        <button className='star' id='three' value={rating} onClick={handleStars}>★</button>
+                        <button className='star' id='four' value={rating} onClick={handleStars}>★</button>
+                        <button className='star' id='five' value={rating} onClick={handleStars}>★</button>
                     <textarea
                         className='review_body'
                         value={body}
