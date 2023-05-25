3903a0461f5c5d61b2d697fafd05507debb89c02
Victor Hou <hou_v@yahoo.com>
2023-05-24 19:32:31 -0700

    defined schema in each migration file.

--- backend/db/migrations/20211011171233-create-restaurants.js
+++ backend/db/migrations/20211011171233-create-restaurants.js
@@ -1,4 +1,10 @@
 'use strict';
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.createTable('Restaurants', {
@@ -38,9 +44,9 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE
       }
-    });
+    }, options);
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Restaurants');
+    return queryInterface.dropTable('Restaurants', options);
   }
 };
