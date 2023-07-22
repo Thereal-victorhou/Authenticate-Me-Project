4782dfec536409b2a7244c921f8e92bc034ba978
Victor Hou <hou_v@yahoo.com>
2023-07-21 18:46:57 -0700

    removed unused pg import from ./src/components/SignupFormPage/signupFormPage.js

--- frontend/src/components/SignupFormPage/signupFormPage.js
+++ frontend/src/components/SignupFormPage/signupFormPage.js
@@ -1,4 +1,3 @@
-import { user } from "pg/lib/defaults";
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
