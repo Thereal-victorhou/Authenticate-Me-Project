f8e735792c46fd6941816e91fda8e11c3ef2aff0
Victor Hou <hou_v@yahoo.com>
2023-07-26 20:50:06 -0700

    worked on frontend rendering for location search.

--- frontend/src/components/Navigation/Navigation.js
+++ frontend/src/components/Navigation/Navigation.js
@@ -12,13 +12,16 @@ function Navigation({ isLoaded }){
   const history = useHistory();
   const dispatch = useDispatch();
 
-  const [searchInput, setSearchInput] = useState('');
+  const [restaurantSearchInput, setRestaurantSearchInput] = useState('');
+  const [locationSearchInput, setLocationSearchInput] = useState('');
   const [isSelected, setIsSelected] = useState(false);
 
   const sessionUser = useSelector(state => state.session.user);
   const searchResult = useSelector(state => state.search)
   const pageType = useSelector(state => state.navigation?.action?.currentPage)
-  const searchInputLength = document.querySelector(".search-bar")?.getAttribute('value')?.length;
+
+  const restaurantSearchInputLength = document.querySelector(".search-bar-restaurants")?.getAttribute('value')?.length;
+  const locationSearchInputLength = document.querySelector(".search-bar-location")?.getAttribute('value')?.length;
 
   // Modifying style of NavBar based on current Page
   useEffect(() => {
@@ -48,32 +51,37 @@ function Navigation({ isLoaded }){
 
   // Live Search
   useEffect(() => {
-    if (searchInput.length > 1) {
+    if (restaurantSearchInput.length > 1) {
       dispatch(liveSearch({
-        searchInput: searchInput,
+        searchInput: restaurantSearchInput,
         userId: sessionUser?.id
       }))
     }
 
-  }, [dispatch, searchInput]);
+  }, [dispatch, restaurantSearchInput]);
 
   // Modifying Live Search Box
   useEffect(() => {
-    if (isSelected && searchInputLength > 1) {
+    if (isSelected && restaurantSearchInputLength > 1) {
       document.querySelector(".search-bar")?.classList.add("live");
 
     } else {
       document.querySelector(".search-bar")?.classList.remove("live");
 
     }
-  },[isSelected && searchInputLength])
+  },[isSelected && restaurantSearchInputLength])
 
-  // Updating Search field
-  const updateSearch= (e) => {
-    setSearchInput(e.target.value);
+  // Updating restaurant search field
+  const updateRestaurantSearch= (e) => {
+    setRestaurantSearchInput(e.target.value);
     if (e.target.value.length === 1) {
       dispatch(clearSearch());
     }
+  }
+
+  // Updating location search field
+  const updateLocationSearch = (e) => {
+
   }
 
   let sessionLinks;
@@ -105,12 +113,12 @@ function Navigation({ isLoaded }){
     e.preventDefault();
     await dispatch(oneRestaurant(res.id));
     await dispatch(clearSearch());
-    setSearchInput('')
+    setRestaurantSearchInput('')
     dispatch(saveCurrentPage('other'))
     history.push(`/restaurants/${res.id}`)
   }
 
-  //Render search results
+  //Render restaurant search results
   const searchRender = (res, i) => {
     return (
       <div id="search-result" key={i} onClick={(e) => handleClick(e, res)}>
@@ -122,6 +130,12 @@ function Navigation({ isLoaded }){
       </div>
     )
   }
+
+  // Render location search results
+  const locationSearchRender = (res, i) => {
+
+  }
+
   // No Results
   const noResult = () => {
     return (
@@ -135,12 +149,13 @@ function Navigation({ isLoaded }){
   const handleSearch = async (e) => {
     e.preventDefault();
     await dispatch(liveSearch({
-      searchInput: searchInput,
+      searchInput: restaurantSearchInput,
       userId: sessionUser?.id
     }))
-    setSearchInput('');
+    setRestaurantSearchInput('');
     dispatch(saveCurrentPage('other'))
-    history.push(`/search?find=${searchInput}`)
+
+    history.push(`/search?find=${restaurantSearchInput}`)
   }
 
   // Set Nav to Home Version
@@ -163,21 +178,26 @@ function Navigation({ isLoaded }){
           <div id="upper-middle">
             <div className="search-bar-container">
               <div className="search-bar-main">
-                <input className="search-bar-restaurants" placeholder="tacos, burgers, dinner" value={searchInput} onChange={updateSearch} onClick={(e) => handleRes(e)}></input>
+                <input className="search-bar-restaurants" placeholder="tacos, burgers, dinner" value={restaurantSearchInput} onChange={updateRestaurantSearch} onClick={(e) => handleRes(e)}></input>
                 <div className="search-bar-divider">
                   <div id="divide"></div>
                 </div>
-                <input className="search-bar-location" placeholder="address, city, state"></input>
+                <input className="search-bar-location" placeholder="address, city, state" value={locationSearchInput} onChange={updateLocationSearch} onClick={(e) => handleRes(e)}></input>
                 <NavLink exact to="/search" className="search-btn" onClick={(e)=> handleSearch(e)}>
                 <p>⌕</p>
                 </NavLink>
               </div>
             </div>
             <div className="search-results-container">
-              {searchResult &&
-                Object.values(searchResult).map((res, i) => (
-                  searchRender(res, i)
-                ))}
+              <div className="restaurant-search-results-container">
+                {searchResult &&
+                  Object.values(searchResult).map((res, i) => (
+                    searchRender(res, i)
+                  ))}
+              </div>
+              <div className="location-search-results-container">
+
+              </div>
             </div>
           </div>
         </div>
@@ -192,38 +212,6 @@ function Navigation({ isLoaded }){
         </div>
       </div>
     </div>
-    // <div className="nav_container" style={{backgroundImage: `url(https://s3-media0.fl.yelpcdn.com/assets/srv0/yelp_large_assets/fa8d73b85ad8/assets/img/home/hero_photos/J4bBEXXBIHmYLl50X1l72g.jpg)`}}>
-    //   <div className='li-container'>
-    //     <div className="nav_container_homelink">
-    //       <div className="homelink_containter">
-    //         <NavLink exact to="/" className="navLinks" id="home-link">
-    //           Tabl
-    //         </NavLink>
-    //         {/* <NavLink exact to={sessionUser ? '/add/restaurant': '/login'} className="navLinks" id="add-restaurant-link">Add a Restaurant</NavLink> */}
-    //       </div>
-    //     </div>
-    //     <div className="middle-container">
-    //       <div id="lower-middle">
-    //         <div className="search-bar-container">
-    //           <input className="search-bar" placeholder="Find Pescatarian, Vegetarian and Vegan..." value={searchInput} onChange={updateSearch} onClick={(e) => handleRes(e)}></input>
-    //           <button id="search-btn" onClick={(e) => handleSearch(e)}>
-    //             <p>⌕</p>
-    //           </button>
-    //         </div>
-    //         <div className="search-results-container">
-    //           {searchResult &&
-    //             Object.values(searchResult).map((res, i) => (
-    //               searchRender(res, i)
-    //             ))}
-    //         </div>
-    //       </div>
-
-    //     </div>
-    //     <div className="session_links">
-    //       {isLoaded && sessionLinks}
-    //     </div>
-    //   </div>
-    // </div>
 
   );
 
