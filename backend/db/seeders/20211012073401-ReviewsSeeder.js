7f0f83a439fde1949882e958b868727c06ef338c
Victor Hou <hou_v@yahoo.com>
2023-08-03 16:59:21 -0700

    modified random review function.

--- backend/db/seeders/20211012073401-ReviewsSeeder.js
+++ backend/db/seeders/20211012073401-ReviewsSeeder.js
@@ -5,14 +5,14 @@ if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;  // define your schema in options object
 }
 
-const reviewBody = [
-  null,
-  "God awful!",
-  "It was alright",
-  "Great Service!",
-  "Lovely atmosphere!",
-  "Amazing food!!!",
-];
+const reviewBody = {
+  '1': 'God awful!',
+  '2': 'It was alright',
+  '3': 'Lovely atmosphere!',
+  '4': 'Great Service!',
+  '5': 'Amazing food!!!',
+}
+
 
 function getRandomNum(min, max) {
   const raw = Math.random() * (max - min) + min;
@@ -23,9 +23,10 @@ function makeReviews(target) {
   const reviews = [];
   for (let i = 2; i <= target; i++) {
     const randomNum = getRandomNum(1, 6);
+
     const newReview = {
-        body: reviewBody[randomNum],
-      userId: getRandomNum(1, 10),
+      body: reviewBody[String(randomNum)],
+      userId: getRandomNum(1, 20),
       restaurantId: getRandomNum(1, 20),
       rating: randomNum,
       createdAt: new Date(),
