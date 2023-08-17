0d95db0fa1fc8c4d43d189ff9b6be1e4a898790f
Victor Hou <hou_v@yahoo.com>
2023-08-17 00:15:11 -0700

    fixed ratings bar rendering

--- frontend/src/components/RestaurantPage/AnimatedRatingDisplay.js
+++ frontend/src/components/RestaurantPage/AnimatedRatingDisplay.js
@@ -1,89 +1,139 @@
 import React, { useEffect, useState, useRef } from 'react';
 
-
 export default function AnimatedRatingDisplay({ reviews }) {
+	const ratingObj = {
+		1: 0,
+		2: 0,
+		3: 0,
+		4: 0,
+		5: 0,
+	};
 
-  const ratingObj = {
-    '1': 0,
-    '2': 0,
-    '3': 0,
-    '4': 0,
-    '5': 0,
-  }
+	const positionRef = useRef(null);
+	const ratingsBarRef1 = useRef(null);
+	const ratingsBarRef2 = useRef(null);
+	const ratingsBarRef3 = useRef(null);
+	const ratingsBarRef4 = useRef(null);
+	const ratingsBarRef5 = useRef(null);
 
-  const positionRef = useRef();
+	const [rating1, setRating1] = useState(0);
+	const [rating2, setRating2] = useState(0);
+	const [rating3, setRating3] = useState(0);
+	const [rating4, setRating4] = useState(0);
+	const [rating5, setRating5] = useState(0);
 
-  const [rating1, setRating1] = useState(0)
-  const [rating2, setRating2] = useState(0)
-  const [rating3, setRating3] = useState(0)
-  const [rating4, setRating4] = useState(0)
-  const [rating5, setRating5] = useState(0)
+	const bars = document.querySelectorAll('.animated-bar');
 
-  const bars = document.querySelectorAll('.animated-bar');
+	reviews.forEach((review) => {
+		if (review.rating === 5) ratingObj['5'] += 1;
+		if (review.rating === 4) ratingObj['4'] += 1;
+		if (review.rating === 3) ratingObj['3'] += 1;
+		if (review.rating === 2) ratingObj['2'] += 1;
+		if (review.rating === 1) ratingObj['1'] += 1;
+	});
 
-  reviews.forEach(review => {
-    if (review.rating === 5) ratingObj['5'] += 1;
-    if (review.rating === 4) ratingObj['4'] += 1;
-    if (review.rating === 3) ratingObj['3'] += 1;
-    if (review.rating === 2) ratingObj['2'] += 1;
-    if (review.rating === 1) ratingObj['1'] += 1;
-  })
+	for (let rating in ratingObj) {
+		const amount = ratingObj[rating];
+		ratingObj[rating] = Math.floor((amount / reviews.length) * 100);
+	}
 
-  for (let rating in ratingObj) {
-    const amount = ratingObj[rating];
-    ratingObj[rating] = Math.floor((amount / reviews.length) * 100);
-  }
+	const showRatings = () => {
+		if (
+			ratingsBarRef5.current &&
+			ratingsBarRef4.current &&
+			ratingsBarRef3.current &&
+			ratingsBarRef2.current &&
+			ratingsBarRef1.current
+		) {
+			ratingsBarRef5.current.style.opacity = '1';
+			ratingsBarRef5.current.style.width = `${ratingObj['5']}%`;
+			ratingsBarRef4.current.style.opacity = '1';
+			ratingsBarRef4.current.style.width = `${ratingObj['4']}%`;
+			ratingsBarRef3.current.style.opacity = '1';
+			ratingsBarRef3.current.style.width = `${ratingObj['3']}%`;
+			ratingsBarRef2.current.style.opacity = '1';
+			ratingsBarRef2.current.style.width = `${ratingObj['2']}%`;
+			ratingsBarRef1.current.style.opacity = '1';
+			ratingsBarRef1.current.style.width = `${ratingObj['1']}%`;
+		}
+	};
 
-  const showRatings = () => {
-    console.log('inside show rating ', bars)
-    bars?.forEach(bar => {
-      const value = bar.value;
-      bar.style.opacity = 1;
-      bar.style.width = `${value}%`;
-      console.log('showRating ====== ', bar)
-    })
+	const hideRatings = () => {
+    if (
+			ratingsBarRef5.current &&
+			ratingsBarRef4.current &&
+			ratingsBarRef3.current &&
+			ratingsBarRef2.current &&
+			ratingsBarRef1.current
+		) {
+			ratingsBarRef5.current.style.opacity = 0;
+			ratingsBarRef5.current.style.width = 0;
+			ratingsBarRef4.current.style.opacity = 0;
+			ratingsBarRef4.current.style.width = 0;
+			ratingsBarRef3.current.style.opacity = 0;
+			ratingsBarRef3.current.style.width = 0;
+			ratingsBarRef2.current.style.opacity = 0;
+			ratingsBarRef2.current.style.width = 0;
+			ratingsBarRef1.current.style.opacity = 0;
+			ratingsBarRef1.current.style.width = 0;
+		}
+	};
 
-  }
+	useEffect(() => {
 
-  const hideRatings = () => {
-    console.log('inside hideRating', bars)
-    bars?.forEach(bar => {
-      bar.style.opacity = 0;
-      bar.style.width = 0;
-      console.log('hideRating ======= ', bar)
-    })
-  }
-
-  useEffect(() => {
     window.addEventListener('scroll', () => {
-      const sectionPos = positionRef.current.getBoundingClientRect().top
-      const screenPos =  window.innerHeight;
+      const sectionPos = positionRef.current ? positionRef.current.getBoundingClientRect().top : null;
+      const screenPos = window.innerHeight;
 
-      if (sectionPos < screenPos) {
-        showRatings()
-      } else {
-        hideRatings()
-      }
-    })
-  }, [])
+      if (sectionPos === null) return;
+			if (sectionPos < screenPos) {
+				showRatings();
+			} else {
+				hideRatings();
+			}
+      return () => window.removeEventListener('scroll', () => {});
+		});
 
-  return (
-    <>
-      <div className='animated-bar-container' ref={positionRef}>
-        <div className='animated-bar' id='animated-bar5' value={`${ratingObj['5']}%`}></div>
-      </div>
-      <div className='animated-bar-container'>
-        <div className='animated-bar' id='animated-bar4' value={`${ratingObj['4']}%`}></div>
-      </div>
-      <div className='animated-bar-container'>
-        <div className='animated-bar' id='animated-bar3' value={`${ratingObj['3']}%`}></div>
-      </div>
-      <div className='animated-bar-container'>
-        <div className='animated-bar' id='animated-bar2' value={`${ratingObj['2']}%`}></div>
-      </div>
-      <div className='animated-bar-container'>
-        <div className='animated-bar' id='animated-bar1' value={`${ratingObj['1']}%`}></div>
-      </div>
-    </>
-  )
+
+	}, []);
+
+	return (
+		<>
+			<div className='animated-bar-container' ref={positionRef}>
+				<div
+					className='animated-bar'
+					id='animated-bar5'
+					ref={ratingsBarRef5}
+				></div>
+			</div>
+			<div className='animated-bar-container'>
+				<div
+					className='animated-bar'
+					id='animated-bar4'
+					ref={ratingsBarRef4}
+					></div>
+			</div>
+			<div className='animated-bar-container'>
+				<div
+					className='animated-bar'
+					id='animated-bar3'
+					ref={ratingsBarRef3}
+				></div>
+			</div>
+			<div className='animated-bar-container'>
+				<div
+					className='animated-bar'
+					id='animated-bar2'
+					ref={ratingsBarRef2}
+					></div>
+			</div>
+			<div className='animated-bar-container'>
+				<div
+					className='animated-bar'
+					id='animated-bar1'
+					ref={ratingsBarRef1}
+        ></div>
+			</div>
+		</>
+	);
 }
