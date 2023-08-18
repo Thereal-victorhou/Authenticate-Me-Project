033340330df31633ec78fb74e570874c42db6ad9
Victor Hou <hou_v@yahoo.com>
2023-08-17 19:57:09 -0700

    created and bug fixed home page animation for review cards

--- frontend/src/components/HomePage/homePage.js
+++ frontend/src/components/HomePage/homePage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 
 import { useSelector, useDispatch } from 'react-redux';
 import { NavLink, useHistory } from 'react-router-dom';
@@ -6,13 +6,139 @@ import { allRestaurants, oneRestaurant } from '../../store/restaurant';
 import { saveCurrentPage } from '../../store/navigation';
 import LiveStarRatingDisplay from '../LiveStarRatingDisplay/LiveStarRatingDisplay';
 
+import VisibilitySensor from 'react-visibility-sensor';
+
 function HomePage({ user }) {
 	const dispatch = useDispatch();
 	const history = useHistory();
 
+	const topContainerRef = useRef(null);
+	const positionRef = useRef(null);
+	const containersRef = useRef([]);
+
+	const [visability, setVisability] = useState(false);
+
+	let container0, container1,container2, container3, container4, container5, container6, container7;
+
 	const sessionRestaurant = useSelector((state) =>
 		Object.values(state.restaurant)
 	);
+
+
+
+	// Animate review cards on scroll
+	useEffect(() => {
+		container0 = containersRef.current[0];
+		container1 = containersRef.current[1];
+		container2 = containersRef.current[2];
+		container3 = containersRef.current[3];
+		container4 = containersRef.current[4];
+		container5 = containersRef.current[5];
+		container6 = containersRef.current[6];
+		container7 = containersRef.current[7];
+
+		window.addEventListener('scroll', () => {
+			const screenPos = window.innerHeight;
+			const sectionPos1 = positionRef.current
+				? positionRef.current.getBoundingClientRect().top
+				: null;
+			const sectionPos2 = container2
+				? container2.getBoundingClientRect().top
+				: null;
+			const sectionPos3 = container4
+				? container4.getBoundingClientRect().top
+				: null;
+			const sectionPos4 = container6
+				? container6.getBoundingClientRect().top
+				: null;
+
+			// Show review cards if user scrolls over top
+			if (
+				sectionPos1 !== null &&
+				container0 !== undefined &&
+				container2 !== undefined &&
+				container4 !== undefined &&
+				container6 !== undefined
+			) {
+				if (sectionPos1 < screenPos) {
+					container0.classList.add('active');
+					container1.classList.add('active');
+
+				} else {
+					container0.classList.remove('active');
+					container0.classList.remove('active');
+				}
+
+				if (sectionPos2 < screenPos) {
+					container2.classList.add('active');
+					container3.classList.add('active');
+
+				} else {
+					container2.classList.remove('active');
+					container3.classList.remove('active');
+				}
+
+				if (sectionPos3 < screenPos) {
+					container4.classList.add('active');
+					container5.classList.add('active');
+
+				} else {
+					container4.classList.remove('active');
+					container5.classList.remove('active');
+				}
+
+				if (sectionPos4 < screenPos) {
+					container6.classList.add('active');
+					container7.classList.add('active');
+
+				} else {
+					container6.classList.remove('active');
+					container7.classList.remove('active');
+				}
+			}
+
+			return () => window.removeEventListener('scroll', () => {});
+		});
+	});
+
+	// Display review cards be default if screen is big enough
+	useEffect(() => {
+
+		if (visability) {
+
+			const screenPos = window.innerHeight;
+			const topContainer = topContainerRef.current ? topContainerRef.current.getBoundingClientRect().top: null;
+			const section1 = container0 ? container0.getBoundingClientRect().top : null;
+			const section2 = container0 ? container0.getBoundingClientRect().top : null;
+			const section3 = container0 ? container0.getBoundingClientRect().top : null;
+			const section4 = container0 ? container0.getBoundingClientRect().top : null;
+			console.log(screenPos, topContainer)
+
+			if (section1 < screenPos) {
+				container0?.classList.add('active')
+				container1?.classList.add('active')
+				console.log('inside 1')
+			}
+
+			if (section2 < screenPos) {
+				container2?.classList.add('active')
+				container3?.classList.add('active')
+				console.log('inside 2')
+			}
+			if (section3 < screenPos) {
+				container4?.classList.add('active')
+				container5?.classList.add('active')
+				console.log('inside 3')
+			}
+
+			if (section4 < screenPos) {
+				container6?.classList.add('active')
+				container7?.classList.add('active')
+				console.log('inside 4')
+			}
+		}
+	})
+
 
 	useEffect(() => {
 		window.scrollTo(0, 0);
@@ -22,49 +148,53 @@ function HomePage({ user }) {
 		dispatch(allRestaurants());
 	}, [dispatch]);
 
+	const checkVis = (isVisable) => {
+		if (isVisable) setVisability(true);
+	}
+
 	const handleNavReview = async (e, restaurant) => {
 		e.preventDefault();
 
 		await dispatch(saveCurrentPage('other'));
 		if (!user) return history.push('/login');
 		return history.push(`/review/restaurant/${restaurant.id}`);
+	};
 
-	}
-
-	const handleNavRestaurant = async(e, restaurant) => {
-		e.preventDefault()
+	const handleNavRestaurant = async (e, restaurant) => {
+		e.preventDefault();
 		e.stopPropagation();
 		await dispatch(oneRestaurant(restaurant.id));
 		dispatch(saveCurrentPage('other'));
 		return history.push(`/restaurants/${restaurant.id}`);
-
-	}
+	};
 
 	return (
-		<div className='all-restaurants-container'>
-			<div className='home-review-title'>
-				<h2>Your New Review Awaits</h2>
-			</div>
+		<div className='all-restaurants-container' ref={topContainerRef}>
+			<VisibilitySensor onChange={checkVis}>
+				<div className='home-review-title'>
+					<h2>Your New Review Awaits</h2>
+				</div>
+			</VisibilitySensor>
 			<div className='review-suggestions-container'>
-				<ul id='card-list'>
+				<ul id='card-list' ref={positionRef}>
 					{sessionRestaurant &&
 						sessionRestaurant.map((restaurant, i) => {
 							return (
-								<li className='restaurant-container' key={restaurant.id}>
+								<li
+									className={`restaurant-container${i}`}
+									key={restaurant.id}
+									ref={(el) => (containersRef.current[i] = el)}>
 									<div
 										className={'restaurant_container'}
 										type='button'
 										// to={`/review/restaurant/${restaurant.id}`}
 										onClick={(e) => {
 											handleNavReview(e, restaurant);
-										}}
-										>
+										}}>
 										<img
 											className={'restaurant-photo'}
 											src={restaurant.imgSrc}
-											alt={'Restaurant Image'}
-
-											></img>
+											alt={'Restaurant Image'}></img>
 										<div className='restaurant-info'>
 											<div
 												className='name-and-location-container'
--- frontend/src/components/RestaurantPage/AnimatedRatingDisplay.js
+++ frontend/src/components/RestaurantPage/AnimatedRatingDisplay.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState, useRef } from 'react';
 
 export default function AnimatedRatingDisplay({ reviews }) {
-
 	const positionRef = useRef(null);
 	const ratingsBarRef1 = useRef(null);
 	const ratingsBarRef2 = useRef(null);
@@ -10,7 +9,7 @@ export default function AnimatedRatingDisplay({ reviews }) {
 	const ratingsBarRef5 = useRef(null);
 
 	let rating1, rating2, rating3, rating4, rating5, showRatings, hideRatings;
-  let maxVal = -Infinity;
+	let maxVal = -Infinity;
 	const ratingObj = {
 		1: 0,
 		2: 0,
@@ -29,15 +28,17 @@ export default function AnimatedRatingDisplay({ reviews }) {
 	// Use ratingObj to track how many reviews for each rating
 	const calculatePercentage = (reviews) => {
 		if (reviews) {
-			reviews.forEach((review) => {           // find quanity of each rating
+			reviews.forEach((review) => {
+				// find quanity of each rating
 				ratingObj[review.rating] += 1;
 			});
 
-			for (let rating in ratingObj) {       // Change values in ratingsObj to percentage
+			for (let rating in ratingObj) {
+				// Change values in ratingsObj to percentage
 				const amount = ratingObj[rating];
-        const percentage = Math.floor((amount / reviews.length) * 100);
+				const percentage = Math.floor((amount / reviews.length) * 100);
 				ratingObj[rating] = percentage;
-        maxVal = percentage === 0 ? 0 : Math.max(maxVal, percentage); // Find and set max percentage
+				maxVal = percentage === 0 ? 0 : Math.max(maxVal, percentage); // Find and set max percentage
 			}
 
 			rating1 = ratingObj['1'];
@@ -45,33 +46,29 @@ export default function AnimatedRatingDisplay({ reviews }) {
 			rating3 = ratingObj['3'];
 			rating4 = ratingObj['4'];
 			rating5 = ratingObj['5'];
-
-
 		}
 	};
 	calculatePercentage(reviews);
 
 	useEffect(() => {
-
 		showRatings = () => {
-
-      refArr.forEach(ref => {
-
-        const val = parseFloat(ref.current.getAttribute('data-value'));
-        const durationForBar = (2 * maxVal) / val;
-        // console.log('duration == ', durationForBar)
-        ref.current.style.opacity = val === 0 ? 0 : '1';
-        ref.current.style.transitionDuration = `${durationForBar === Infinity ? 0 : durationForBar}s`;
-        ref.current.style.width = `${val}%`;
-
-      })
+			refArr.forEach((ref) => {
+				const val = parseFloat(ref.current.getAttribute('data-value'));
+				const durationForBar = ( val/maxVal ) * 2000
+				// console.log(ref.current.id, ' == duration == ', durationForBar, ' max === ', maxVal, ' val=== ', val)
+				ref.current.style.opacity = val === 0 ? 0 : '1';
+				ref.current.style.transitionDuration = `${
+					durationForBar === Infinity ? 0 : durationForBar
+				}ms`;
+				ref.current.style.width = `${val}%`;
+			});
 		};
 
 		hideRatings = () => {
-      refArr.forEach(ref => {
-        ref.current.style.opacity = 0;
-        ref.current.style.width = 0;
-      })
+			refArr.forEach((ref) => {
+				ref.current.style.opacity = 0;
+				ref.current.style.width = 0;
+			});
 		};
 
 		window.addEventListener('scroll', () => {
@@ -87,8 +84,7 @@ export default function AnimatedRatingDisplay({ reviews }) {
 					hideRatings();
 				}
 			}
-			return () =>
-				window.removeEventListener('scroll', () => {});
+			return () => window.removeEventListener('scroll', () => {});
 		});
 	});
 
@@ -98,35 +94,35 @@ export default function AnimatedRatingDisplay({ reviews }) {
 				<div
 					className='animated-bar'
 					id='animated-bar5'
-          data-value={rating5}
+					data-value={rating5}
 					ref={ratingsBarRef5}></div>
 			</div>
 			<div className='animated-bar-container'>
 				<div
 					className='animated-bar'
 					id='animated-bar4'
-          data-value={rating4}
+					data-value={rating4}
 					ref={ratingsBarRef4}></div>
 			</div>
 			<div className='animated-bar-container'>
 				<div
 					className='animated-bar'
 					id='animated-bar3'
-          data-value={rating3}
+					data-value={rating3}
 					ref={ratingsBarRef3}></div>
 			</div>
 			<div className='animated-bar-container'>
 				<div
 					className='animated-bar'
 					id='animated-bar2'
-          data-value={rating2}
+					data-value={rating2}
 					ref={ratingsBarRef2}></div>
 			</div>
 			<div className='animated-bar-container'>
 				<div
 					className='animated-bar'
 					id='animated-bar1'
-          data-value={rating1}
+					data-value={rating1}
 					ref={ratingsBarRef1}></div>
 			</div>
 		</>
