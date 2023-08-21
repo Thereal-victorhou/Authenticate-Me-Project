9b115afe56d1e3c6b40a58519941d90eaa6e6473
Victor Hou <hou_v@yahoo.com>
2023-08-20 17:56:49 -0700

    added numeric glyphs for restaurant icons and added info window  in results maps.

--- frontend/src/components/SearchResultPage/resultsMap.js
+++ frontend/src/components/SearchResultPage/resultsMap.js
@@ -8,7 +8,7 @@ function ResultsMap({ searchResults, location }) {
 		const center = { lat: location.lat, lng: location.lng };
 		// Request needed libraries.
 		//@ts-ignore
-		const { Map } = await window.google.maps.importLibrary('maps');
+		const { Map, InfoWindow } = await window.google.maps.importLibrary('maps');
 		const { AdvancedMarkerElement, PinElement } =
 			await window.google.maps.importLibrary('marker');
 
@@ -21,29 +21,44 @@ function ResultsMap({ searchResults, location }) {
 			mapId: 'ff8dbb61c8194218',
 		});
 
+    const infoWindow = new InfoWindow();
+
 		const restaurantLocations = [];
+
 		if (searchResults)
-			searchResults.forEach((res, i) => {
+			searchResults.forEach((res) => {
 
 				restaurantLocations.push({
 					coordinates: { lat: Number(res.coordinates[0]), lng: Number(res.coordinates[1]) },
-					title: `${res.location[0]}`,
+					title: `${res.name}`,
 				});
 			});
 
-
-    const addMarkers = (locationObj) => {
+    // Create markers and in glyphs to them
+    const addMarkers = (locationObj, i) => {
+      const pin = new PinElement({
+        glyph: `${i + 1}`,
+      });
       const marker = new AdvancedMarkerElement({
         map: map,
         position: locationObj.coordinates,
         title: locationObj.title,
+        content: pin.element
+      });
+      // Listen for clicks on markers to show info window
+      marker.addListener("click", ({ domEvent, latLng }) => {
+        const { target } = domEvent;
+
+        infoWindow.close();
+        infoWindow.setContent(marker.title);
+        infoWindow.open(marker.map, marker);
       });
 
       bounds.extend(marker.position)
     }
 
     for (let i = 0; i < restaurantLocations.length; i++) {
-      addMarkers(restaurantLocations[i])
+      addMarkers(restaurantLocations[i], i)
     }
 
     map.fitBounds(bounds);
