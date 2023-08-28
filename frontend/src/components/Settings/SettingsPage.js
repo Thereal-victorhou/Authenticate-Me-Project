33b990a3e93dd2c6c9bf53d320766727273fc871
Victor Hou <hou_v@yahoo.com>
2023-08-28 00:05:50 -0700

    created main skeleton for settings page.

--- frontend/src/components/Settings/SettingsPage.js
+++ frontend/src/components/Settings/SettingsPage.js
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from "react";
+import './SettingsPage.css';
 
 function SettingsPage() {
 
 
   return (
     <>
-      <div>
-        Welcome to Settings Page
+      <div className="settings-page-main">
+        <div className="settings-main-content">
+          <div className='settings'></div>
+          <div className='content'></div>
+        </div>
       </div>
     </>
   )
